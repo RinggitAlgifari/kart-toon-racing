b186c2564c65b13f5284d51874ef9d6116942277
Adam <madfaladam@gmail.com>
2022-04-06 14:46:54 +0700

    add script slider bgm and sfx

--- Kart Toon Racing/Assets/Scripts/MenuManager.cs
+++ Kart Toon Racing/Assets/Scripts/MenuManager.cs
@@ -1,15 +1,38 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
     // Start is called before the first frame update\
     public GameObject AccelObj, ArcadePanel, AccelPanel;
+    [Header("[ audio ]")]
+    [SerializeField] Slider sliderBGM;
+    [SerializeField] Slider sliderSFX;
+
     void Start()
     {
-        
+        sliderBGM.onValueChanged.AddListener(valueChangeBGM);
+        sliderSFX.onValueChanged.AddListener(valueChangeSFX);
     }
+
+    private void OnEnable()
+    {
+        sliderBGM.value = SoundS.Instance.GetBgmVol();
+        sliderSFX.value = SoundS.Instance.GetSfxVol();
+    }
+
+    #region audio
+    private void valueChangeSFX(float v)
+    {
+        SoundS.Instance.SetSFXMVol(v);
+    }
+
+    private void valueChangeBGM(float v)
+    {
+        SoundS.Instance.SetBGMVol(v);
+    }
+    #endregion
 
     // Update is called once per frame
     void Update()
---
+++ Kart Toon Racing/Assets/Scripts/SoundS.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundS : MonoBehaviour
+{
+    public static SoundS Instance;
+
+    [SerializeField] AudioMixer audioMixer;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    #region Get
+
+    public float GetBgmVol()
+    {
+        float vol = 0f;
+        audioMixer.GetFloat("BgmVol", out vol);
+        return vol;
+    }
+
+    public float GetSfxVol()
+    {
+        float vol = 0f;
+        audioMixer.GetFloat("SfxVol", out vol);
+        return vol;
+    }
+    #endregion
+
+    public void SetBGMVol(float v)
+    {
+        audioMixer.SetFloat("BgmVol", v);
+    }
+    public void SetSFXMVol(float v)
+    {
+        audioMixer.SetFloat("SfxVol", v);
+    }
+}

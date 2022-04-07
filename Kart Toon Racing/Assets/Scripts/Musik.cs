3da962adc7d37bad00d524a7a7ff444d3c17f3e3
Adam <madfaladam@gmail.com>
2022-04-07 10:39:03 +0700

    arrange asset and update sound BGM

--- Kart Toon Racing/Assets/Scripts/Musik.cs
+++ Kart Toon Racing/Assets/Scripts/Musik.cs
@@ -7,14 +7,14 @@ public class Musik : MonoBehaviour
     void Awake()
     {
         StartCoroutine(LoadKeMenu());
-        GameObject[] objs = GameObject.FindGameObjectsWithTag("music");
+        /*GameObject[] objs = GameObject.FindGameObjectsWithTag("music");
 
         if (objs.Length > 1)
         {
             Destroy(this.gameObject);
         }
 
-        DontDestroyOnLoad(this.gameObject);
+        DontDestroyOnLoad(this.gameObject);*/
     }
 
     IEnumerator LoadKeMenu(){
